86583bcfd47c0fdda3c2e732b8c99d70835ffe60
DoVanTuann <129986145+tuando18@users.noreply.github.com>
2024-07-25 00:28:36 +0700

    Update paypalRoutes.js

--- router/paypalRoutes.js
+++ router/paypalRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 paypal.configure({
   mode: 'sandbox',
-  client_id: 'AdnsK6zTAwVD5I_Fm0Zz-yyIdkyOi4xvvn9sMKL4YwDEpcmqe6kW7XRTbpLfxRzfZ7GSR3H3mjoLJrKR',
-  client_secret: 'ECPgU9rV0kmXYn8SwHk_U715hU5Pt_dJYoqjx3Lc7tL64KCTHhrHCVTG7e_nvCdjVw_KvVNxNJ3qXTHm',
+  // client_id: 'AdnsK6zTAwVD5I_Fm0Zz-yyIdkyOi4xvvn9sMKL4YwDEpcmqe6kW7XRTbpLfxRzfZ7GSR3H3mjoLJrKR',
+  // client_secret: 'ECPgU9rV0kmXYn8SwHk_U715hU5Pt_dJYoqjx3Lc7tL64KCTHhrHCVTG7e_nvCdjVw_KvVNxNJ3qXTHm',
 });
 
 const exchangeRate = 0.000043;
